a83f429950b7ba0f70b6137e71257e0685a35ec4
Gabriella Bueno <gabriellacbueno@outlook.com>
2024-04-28 23:03:58 -0300

    fix: cat to ghost image transition

--- src/script.js
+++ src/script.js
@@ -2,7 +2,7 @@ const cat = document.querySelector(".cat");
 const block = document.querySelector(".block");
 const gameOver = document.querySelector(".hidden");
 
-// Adiciona e remove classe jump; animaçao para o gato pular
+// Adiciona e remove classe jump; animação para o gato pular
 const jump = () => {
   cat.classList.add("jump");
 
@@ -17,12 +17,12 @@ const loop = setInterval(() => {
 
   // Valores para condição de parada(game over)
   const blockMeetsCat = 250; // Quando o bloco encosta no gato
-  const catSafeJump = 70; // Valor mínimo antes do gato encostar no bloco
+  const catSafeJump = 30; // Valor mínimo antes do gato encostar no bloco
 
   const blockPosition = block.offsetLeft; // Acessa valor 'left' do css do bloco
   const catPosition = +window.getComputedStyle(cat).bottom.replace("px", "");
   /// não existe função offsetBottom
-  /// por isso se utiliza 'getComputedStyle()' para acessar estilos da classe gato
+  /// 'getComputedStyle()' para acessar estilos da classe gato
   /// função 'replace()' para transformar string (px) em número
 
   // GAME OVER; condição de parada do jogo
@@ -34,22 +34,23 @@ const loop = setInterval(() => {
     // Paralisa animações
     block.style.animation = "none";
      
-
     // Fixa elementos na posição
     block.style.left = `${blockPosition}px`;
     cat.style.bottom = `${catPosition}px`;
 
     // Muda imagem do gato para fantasma; ajusta proporções
+    cat.style.opacity = "0.70";
     cat.src = "./images/ghost.gif";
+    //cat.style.opacity = "1";
     cat.style.width = "80px";
     cat.style.marginLeft = "80px";
 
     // Animação fantasma flutuando
-    cat.style.animation = "ghost-animation 2s infinite linear";
+    cat.style.animation = "ghost-floating-animation 2s infinite linear";
     
-    // Mostra tela de game over
+    // Mostra tela de Game Over
     gameOver.classList.remove("hidden");
-
+    
     clearInterval(loop); // Encerra loop
   }
 }, 10);
